18124fc34eb393ffaca730c980d1502554d711e6
Nikolai Mushegian <nikolai@bitshares.org>
2014-06-19 19:20:28 -0400

    update dns ops

--- libraries/blockchain/include/bts/blockchain/dns_operations.hpp
+++ libraries/blockchain/include/bts/blockchain/dns_operations.hpp
@@ -3,22 +3,37 @@
 namespace bts { namespace blockchain {
     struct update_domain_data_operation
     {
+        update_domain_data_operation():domain_name(""){};
+
         static const operation_type_enum type;
+
         address      owner;
         string       domain_name;
         variant      value;
+
+        void evaluate( transaction_evaluation_state& eval_state );
     };
 
     struct update_auction_data_operation
     {
+        update_domain_auction_data_operation():domain_name(""),bid(0){};
         enum auction_update_type
         {
             sell = 0,
             bid = 1
         }
-        string      domain_name;
-        address     bidder;
-        asset       bid;
+
+        static const operation_type_enum   type
+
+        string                             domain_name;
+        address                            bidder;
+        asset                              bid;
+        auction_update_type                update_type;
+
+        void evaluate( transaction_evaluation_state& eval_state );
     }
 
 }}; // bts::blockchain
+
+FC_REFLECT( bts::blockchain::update_domain_data_operation, (update_type)(owner)(domain_name)(value) );
+FC_REFLECT( bts::blockchain::update_auction_data_operation, (domain_name)(bidder)(bid) );
